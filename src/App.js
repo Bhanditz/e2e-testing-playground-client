0bb2c885a76af5a52c54ac6af55dda40928aebdf
Daniele Salvatore <danieleslvtr@gmail.com>
2017-10-15 17:54:23 +0200

    Add axios and get user name from API call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Jo"
+    };
+  }
+
+  componentDidMount(){
+
+    axios.get('http://localhost:3001/api')
+      .then( (response) => {
+        this.setState(response.data);
+      })
+      .catch( (error) =>{
+        console.log(error);
+      });
+
+  }
+
   render() {
+    const {name} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome {name}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
