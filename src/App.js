030039e12455f63e53af8b2b77b63b7c20f5a21b
Daniele Salvatore <danieleslvtr@gmail.com>
2017-10-17 17:21:04 +0200

    Change homepage structure to ease cypress test

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome {name}</h1>
+          <h1 className="App-title">Welcome!</h1>
+          <span id="name">{name}</span>  
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
